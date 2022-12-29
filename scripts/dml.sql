c1d496a360556d6f1bce3d2ec0949e2a305a6b8c
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2022-12-28 23:58:32 -0300

    Povoando região e mapa

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -5,5 +5,15 @@ values
 ('Prédio Lex Luthor'),
 ('Avenida Mulher Maravilha'),
 ('Super-man Land'),
-('Estádio Flash');
+('Estádio Flash'),
+('Rua Detetive Gordon');
+
+insert into tb_regiao(nome, posicao_x, posicao_y, localmapa)
+values
+('Gotham', 1, 1, 1),
+('Metropoles', 2, 1, 2),
+('Metropoles', 2, 4, 3),
+('Metropoles', 2, 7, 4),
+('Metropoles', 2, 8, 5),
+('Gotham', 1, 4, 6);
 
