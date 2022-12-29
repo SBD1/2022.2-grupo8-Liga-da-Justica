3ba6a9f3b9c6d29831332f26bd1978629ee98102
MtsSrs <matheus.1503@hotmail.com>
2022-12-29 17:23:35 -0300

    Adição da normalização do dml e dll

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -88,3 +88,159 @@ create table tb_amazonas(
 create table tb_alien(
 
 ) inherits(tb_raca);
+
+CREATE TABLE faccao (
+    ID_faccao serial not null,
+    Nome varchar(30) not null,
+    MULT_honra int not null,
+    personagem int not null,
+
+    CONSTRAINT pk_faccao primary key (ID_faccao),
+    CONSTRAINT sk_faccao unique (Nome),
+    CONSTRAINT fk_personagem FOREIGN KEY (personagem) REFERENCES tb_personagem (id_personagem)
+);
+
+-- Heroi 
+CREATE TABLE heroi(
+
+) inherits(faccao);
+
+-- Vilao
+CREATE TABLE vilao(
+
+) inherits(faccao);
+
+-- Classe
+CREATE TABLE classe (
+    ID_Classe serial primary key,
+    CONSTRAINT fk_personagem FOREIGN KEY (personagem) REFERENCES tb_personagem (id_personagem)
+);
+
+-- Velocista
+CREATE TABLE velocista(
+    IND_Percepcao int not null,
+    IND_Velocidade int not null
+) inherits(classe);
+
+-- Magico
+CREATE TABLE Magico(
+    IND_Magia int not null,
+    IND_Carisma int not null
+) inherits(classe);
+
+-- Combatente
+CREATE TABLE Combatente(
+    IND_Forca int not null,
+    IND_Velocidade int not null
+)inherits(classe);
+
+-- Detetive
+CREATE TABLE Detetive(
+    IND_Furtividade int not null,
+    IND_Carisma int not null,
+    IND_percepcao int not null
+)inherits(classe);
+
+-- Brutamonte
+CREATE TABLE Brutamonte(
+    IND_Forca int not null,
+    IND_Velocidade int not null,
+)inherits(classe);
+
+-- Item
+create table tb_item(
+id serial constraint pk_id_item primary key,
+nome varchar(50) not null,
+descricao varchar(100),
+valor int,
+min_level int
+);
+
+-- Equipamento
+create table tb_equipamento(
+tipo varchar(10)
+) inherits(tb_item);
+
+-- Consumivel
+create table tb_consumivel(
+MULT_ataque int,
+MULT_poder int,
+MULT_qtd_PontosDeVida int ,
+MULT_qtd_PonstosDeStamina int
+) inherits(tb_item);
+
+-- Arma
+create table tb_Arma(
+MULT_ataque int
+) inherits(tb_equipamento);
+
+-- Armadura
+create table tb_Armadura(
+MULT_defesa int
+) inherits(tb_equipamento);
+
+-- Acessorio
+create table tb_Acessorio(
+MULT_Poder int
+) inherits(tb_equipamento);
+
+-- Instancia do Item
+create table tb_instancia_item (
+id serial not null,
+id_item int not null,
+constraint pk_instancia_item primary key (id)
+);
+
+-- Inventario
+create table tb_inventario(
+id serial not null, 
+capacidade int DEFAULT 100 NOT NULL,
+qtd_atual int, 
+qtd_dinheiro int,
+id_item int,
+id_personagem int not null,
+constraint pk_iventario primary key(id),
+constraint fk_id_intem foreign key(id_item) references tb_instancia_item (id)
+constraint fk_personagem foreign key (id_personagem) references tb_personagem (id)
+);
+
+-- Missao
+create table tb_missao (
+	id serial constraint pk_id_missao primary key,
+	nivel_min int,
+	REQ_Faccao boolean,
+	id_personagem int,
+	constraint fk_personagem foreign key (id_personagem) references tb_personagem (id)
+)
+
+-- npc
+CREATE TYPE multiply_poder AS ENUM ('1', '2', '3', '4', '5');
+
+create table tb_npc(
+	id serial constraint pk_id_npc primary key,
+	nome varchar(30) not null,
+	id_personagem int not null,
+	constraint fk_personagem foreign key (id_personagem) references tb_personagem (id)
+);
+
+-- npc mercador
+create table tb_npc_mercador(
+	tipo varchar(30)
+) inherits (tb_npc);
+
+-- npc ajudante
+create table tb_npc_ajudante(
+	vida int not null,
+	dano int not null
+) inherits (tb_npc);
+
+-- npc mentor
+create table tb_npc_mentor(
+	MULT_poder multiply_poder
+) inherits (tb_npc);
+
+-- npc inimigo
+create table tb_npc_inimigo(
+	vida int not null,
+	dano int not null
+) inherits (tb_npc);
--- scripts/dml.sql
+++ scripts/dml.sql
@@ -8,7 +8,7 @@ values
 ('Estádio Flash'),
 ('Rua Detetive Gordon');
 
--Povoando a tabela de região
+-- Povoando a tabela de região
 insert into tb_regiao(nome, posicao_x, posicao_y, localmapa)
 values
 ('Gotham', 1, 1, 1),
@@ -18,3 +18,91 @@ values
 ('Metropoles', 2, 8, 5),
 ('Gotham', 1, 4, 6);
 
+-- Povoando a tabela das classes
+insert into velocista(id_classe,personagem,ind_percepcao,ind_velocidade) values(1,1,45,70);
+insert into velocista(id_classe,personagem,ind_percepcao,ind_velocidade) values(1,2,45,70);
+
+insert into brutamonte(id_classe,personagem,ind_forca,ind_velocidade) values(2,3,70,20);
+insert into brutamonte(id_classe,personagem,ind_forca,ind_velocidade) values(2,4,70,20);
+insert into brutamonte(id_classe,personagem,ind_forca,ind_velocidade) values(2,5,70,20);
+
+insert into detetive(id_classe,personagem,IND_Furtividade,IND_Carisma,ind_percepcao) values(3,6,70,40,40);
+insert into detetive(id_classe,personagem,IND_Furtividade,IND_Carisma,ind_percepcao) values(3,7,70,40,40);
+insert into detetive(id_classe,personagem,IND_Furtividade,IND_Carisma,ind_percepcao) values(3,8,70,40,40);
+
+insert into magico(id_classe,personagem,IND_Magia, IND_Carisma) values(4,9,70,40);
+insert into magico(id_classe,personagem,IND_Magia, IND_Carisma) values(4,10,70,40);
+
+insert into combatente(id_classe,personagem, ind_forca,ind_velocidade) values (5,11,60,40);
+
+-- Povoando Itens
+
+insert into tb_instancia_item (id_item)
+select id from tb_item
+
+insert into tb_inventario (capacidade, qtd_atual,qtd_dinheiro,id_item, id_personagem)
+values (99,1,50,5,1), (98,2,50,9,1), (97,3,51,17,1), (99,1,32,4,2), (98,2,36,18,2)
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Espada','Arma branca constituída por uma longa lâmina de aço', 40, 3, 'Arma', 4);
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Pistola','Arma de fogo portátil', 60, 2, 'Arma', 5);
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Bastão Longo','Arma branca muito utilizada por lutadores de artes marciais', 20, 1, 'Arma', 3);
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Tridente','Arma branca de três pontas originada de atlântida', 0, 6, 'Arma', 9);
+
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Arco e flecha','Arma de longo alcance', 15, 1, 'Arma', 2);
+
+insert into tb_arma (nome, descricao, valor,min_level,tipo, mult_ataque)
+Values('Soco inglês','Arma branca utilizada para aumentar o poder do seu soco', 10, 1, 'Arma', 1);
+
+insert into tb_armadura  (nome, descricao, valor,min_level,tipo, mult_defesa)
+Values('Capacete de combate','proteção na cabeça utilizada por soldados', 15, 2, 'Armadura', 2);
+
+insert into tb_armadura  (nome, descricao, valor,min_level,tipo, mult_defesa)
+Values('Colete Balístico','proteção no tronco para evitar tiros', 15, 1, 'Armadura', 3);
+
+insert into tb_armadura  (nome, descricao, valor,min_level,tipo, mult_defesa)
+Values('Armadura completa balística','Roupa completa feita de um tecido aprova de balas', 70, 4, 'Armadura', 6);
+
+insert into tb_armadura  (nome, descricao, valor,min_level,tipo, mult_defesa)
+Values('Amvabraço de Titânio ','Proteção da parte inferior do antebraço', 40, 1, 'Armadura', 2);
+
+insert into tb_armadura  (nome, descricao, valor,min_level,tipo, mult_defesa)
+Values('Joelheira comum','Proteção dos joelhos, muito utilizada para evitar quedas', 5, 1, 'Armadura', 1);
+
+insert into tb_acessorio  (nome, descricao, valor,min_level,tipo, mult_poder)
+Values('Capa usada do Batman','Capa utilizada pelo cavaleiro das trevas no seu antigo uniforme', 50, 1, 'Acessorio', 5);
+
+insert into tb_acessorio  (nome, descricao, valor,min_level,tipo, mult_poder)
+Values('Capa usada do Superman','Capa utilizada pelo Superman no seu antigo uniforme', 80, 1, 'Acessorio', 2);
+
+insert into tb_acessorio  (nome, descricao, valor,min_level,tipo, mult_poder)
+Values('Braceletes usados da Mulher-maravilha','Bracelete utilizado pela heroína no seu antigo uniforme', 60, 1, 'Acessorio', 3);
+
+insert into tb_acessorio  (nome, descricao, valor,min_level,tipo, mult_poder)
+Values('Máscara capanga coringa','Adereço utilizado pela gangue do coringa para assutar os cidadãos', 10, 1, 'Acessorio', 1);
+
+insert into tb_acessorio  (nome, descricao, valor,min_level,tipo, mult_poder)
+Values('Cartola mágica','Chapéu encantado por magias desconhecidas', 100, 3, 'Acessorio', 8);
+
+insert into tb_consumivel  (nome, descricao, valor,min_level, mult_ataque, mult_poder, mult_qtd_pontosdevida, mult_qtd_ponstosdestamina)
+Values('Poção de vida','Mistura de ingredientes que aumentam a vida de quem tomar', 10, 1, 0, 0, 2, 0);
+
+insert into tb_consumivel  (nome, descricao, valor,min_level, mult_ataque, mult_poder, mult_qtd_pontosdevida, mult_qtd_ponstosdestamina)
+Values('Tônico','aumenta sua stamina', 10, 1, 0, 0, 0, 2);
+
+insert into tb_consumivel  (nome, descricao, valor,min_level, mult_ataque, mult_poder, mult_qtd_pontosdevida, mult_qtd_ponstosdestamina)
+Values('Injeção de andrenalina','aumenta sua stamina', 30, 2, 2, 4, -1, 0);
+
+insert into tb_consumivel  (nome, descricao, valor,min_level, mult_ataque, mult_poder, mult_qtd_pontosdevida, mult_qtd_ponstosdestamina)
+Values('Poção mágica','Mistura de ingredientes que aumentam o poder mágico de quem tomar', 50, 1, 0, 4, 0, 0);
+
+insert into tb_consumivel  (nome, descricao, valor,min_level, mult_ataque, mult_poder, mult_qtd_pontosdevida, mult_qtd_ponstosdestamina)
+Values('Band-aid','curativo para ferimentos', 3, 1, 0, 0, 2, 0);
