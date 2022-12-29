72afd0b5c74d6efcbffb141ac4c531b2fa42ff71
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2022-12-28 23:45:34 -0300

    Update dml.sql

---
+++ scripts/dml.sql
@@ -0,0 +1,9 @@
+-- Povoando a tabela do mapa
+insert into tb_mapa(nome)
+values
+('Beco do Batman'),
+('Prédio Lex Luthor'),
+('Avenida Mulher Maravilha'),
+('Super-man Land'),
+('Estádio Flash');
+
