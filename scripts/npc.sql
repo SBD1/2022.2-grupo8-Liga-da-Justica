4db2266ceaafc4e89a69487b6b281afce9e38a7f
MtsSrs <81986235+MtsSrs@users.noreply.github.com>
2022-12-29 00:33:01 -0300

    Update npc.sql

--- scripts/npc.sql
+++ scripts/npc.sql
@@ -5,22 +5,22 @@ create table tb_npc(
 	nome varchar(30) not null,
 	id_personagem int not null,
 	constraint fk_personagem foreign key (id_personagem) references tb_personagem (id)
-)
+);
 
 create table tb_npc_mercador(
 	tipo varchar(30)
-) inherits (tb_npc)
+) inherits (tb_npc);
 
 create table tb_npc_ajudante(
 	vida int not null,
 	dano int not null
-) inherits (tb_npc)
+) inherits (tb_npc);
 
 create table tb_npc_mentor(
 	MULT_poder multiply_poder
-) inherits (tb_npc)
+) inherits (tb_npc);
 
 create table tb_npc_inimigo(
 	vida int not null,
 	dano int not null
-) inherits (tb_npc)
+) inherits (tb_npc);
