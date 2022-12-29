11f2862012b81f7f94666558da88a3308d9f4b74
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2022-12-28 22:41:45 -0300

    Update raça inherits and foreign key

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,27 +1,26 @@
--- For regiao type
-create TYPE nome_regiao AS ENUM (
-    'Gotham', 'Metropoles'
-);
-
--- Região
-CREATE TABLE TB_REGIAO(
-	id serial constraint pk_ID_Regiao primary key,
-   	Nome nome_regiao not null ,
-   	posicao_x int not null,
-   	posicao_y int not null,
-   	LocalMapa varchar(1) not null
- 
-);
-
 --Mapa
-CREATE TABLE TB_MAPA(
+create table tb_mapa(
 	id serial constraint pk_ID_Mapa primary key,
    	Nome varchar(33) not null
 );
 
+-- For regiao type
+create type  nome_regiao AS ENUM (
+    'Gotham', 'Metropoles'
+);
+
+-- Região
+create table tb_regiao(
+	id serial constraint pk_ID_Regiao primary key,
+   	Nome nome_regiao not null ,
+   	posicao_x int not null,
+   	posicao_y int not null,
+   	LocalMapa int not null,
+   	constraint  fk_LocalMapa foreign key(LocalMapa)  references tb_mapa (id)
+);
 
 -- Raça
-CREATE TABLE TB_RACA(
+create table tb_raca(
    id serial constraint pk_ID_Raca primary key,
    Nome varchar(10) not null,
    IND_velocidade int not null,
@@ -30,5 +29,31 @@ CREATE TABLE TB_RACA(
    IND_forca int not null,
    IND_carisma int not null,
    IND_percepcao int not null,
-   personagem int not null
+   id_personagem int not null,
+   constraint fk_personagem foreign key(id_personagem) references tb_personagem (id)
 );
+
+-- Humano
+create table tb_humano(
+
+) inherits(tb_raca);
+
+-- Homi-Magi
+create table tb_homi_magi(
+
+) inherits(tb_raca);
+
+-- Atlante
+create table tb_atlante(
+
+) inherits(tb_raca);
+
+-- Amazonas
+create table tb_amazonas(
+
+) inherits(tb_raca);
+
+-- Alien 
+create table tb_alien(
+
+) inherits(tb_raca);
