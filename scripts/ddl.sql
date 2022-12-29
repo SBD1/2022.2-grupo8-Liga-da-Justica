970463e9acd6f5e3fec6a66292b2e9935083b710
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2022-12-28 23:36:51 -0300

    Update ddl - add poder and personagem

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -1,7 +1,7 @@
 --Mapa
 create table tb_mapa(
-	id serial constraint pk_ID_Mapa primary key,
-   	Nome varchar(33) not null
+	id serial constraint pk_id_mapa primary key,
+   	nome varchar(33) not null
 );
 
 -- For regiao type
@@ -11,18 +11,49 @@ create type  nome_regiao AS ENUM (
 
 -- Região
 create table tb_regiao(
-	id serial constraint pk_ID_Regiao primary key,
-   	Nome nome_regiao not null ,
+	id serial constraint pk_id_regiao primary key,
+   	nome nome_regiao not null ,
    	posicao_x int not null,
    	posicao_y int not null,
-   	LocalMapa int not null,
-   	constraint  fk_LocalMapa foreign key(LocalMapa)  references tb_mapa (id)
+   	localMapa int not null,
+   	constraint  fk_LocalMapa foreign key(localMapa)  references tb_mapa (id)
+);
+
+--poder
+create table tb_poderes(
+	id serial constraint pk_id_poder primary key,
+	Velocidade int,
+	Furtividade int,
+	Magia int,
+	Força int,
+	Carisma int,
+	Percepção int
+);
+
+-- Personagem
+create table tb_personagem(
+	id serial constraint pk_id_personagem primary key,
+	experiencia int not null,
+	nivel int not null,
+	nome varchar(30) not null,
+	QTD_PontosDeVida int not null,
+	MAX_PontoDeVida int not null,
+	sexo int not null,
+	QTD_PontosDeEstamina int not null,
+	MAX_PontosDeEstamina int not null,
+	QTD_Honra int not null,
+	QTD_Defesa int not null,
+	QTD_Ataque int not null,
+	poderes int not null,
+	constraint fk_poderes foreign key(poderes) references tb_poderes (id),
+	Local_Atual int not null,
+	constraint fk_Local_Atual foreign key(Local_Atual) references tb_regiao (id)
 );
 
 -- Raça
 create table tb_raca(
-   id serial constraint pk_ID_Raca primary key,
-   Nome varchar(10) not null,
+   id serial constraint pk_id_raca primary key,
+   nome varchar(10) not null,
    IND_velocidade int not null,
    IND_furtividade int not null,
    IND_magia int not null,
