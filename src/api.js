48cd07c1ce46f9a6457d1270c6513f2f864118ad
CorreiaJV <correia.jv16@gmail.com>
2022-12-30 16:14:36 -0300

    Adição movimento entre duas regiões

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const readline = require('readline-sync');
 
 class Player{
     constructor(id,nome, id_localAtual, pos_x, pos_y){
-        this.id=id,
+        this.id=id;
         this.nome= nome;
         this.id_localAtual = id_localAtual;
         this.pos_x = pos_x;
@@ -66,10 +66,6 @@ class Api{
 
 
 
-
-
-
-
 function askAndReturn(texto){
     return readline.question(texto)
 }
@@ -81,7 +77,7 @@ const r = askAndReturn('1- Jogar\n2- Sair\n')
 let player = new Player()
 let posicao= 0
 let username =''
-
+let regiao=''
     if (r == 2) {
         process.exit()
     }
@@ -99,12 +95,13 @@ let username =''
                 console.log("Bem-vindo " + player.nome)
                 break;
              } catch (error) {
-                 console.log(error)
+                 console.log("Personagem não encontrado")
              }
 
         }
 
         while(aux!=0){
+            let m =0
             try {
                 result=  await api.getLogin(username)
                 posicao = await api.getPosicao(result.id_local_atual)
@@ -112,12 +109,20 @@ let username =''
                 console.log(error)
             }            
             console.log(`Você está em '${posicao.nome} (${posicao.posicao_x}, ${posicao.posicao_y})'`)
-            const m= askAndReturn('1- Andar para frente\n2- Andar para trás\n3- Andar para cima\n4- Andar para baixo\n')
-            console.log(m)
+            
+            if (posicao.posicao_x == 4 && posicao.posicao_y ==4 && posicao.nome != "QG da liga"){
+                m= askAndReturn('1- Andar para frente\n2- Andar para trás\n3- Andar para cima\n4- Andar para baixo\n5- Ir para próxima região\n')
+                regiao = "QG da liga"
+            }else if(posicao.posicao_x == 0 && posicao.posicao_y ==0 && posicao.nome != "Arkham city") {
+                m= askAndReturn('1- Andar para frente\n2- Andar para trás\n3- Andar para cima\n4- Andar para baixo\n5- Voltar para a região anterior\n')
+                regiao = "Arkham city"
+            }
+            else{
+                m= askAndReturn('1- Andar para frente\n2- Andar para trás\n3- Andar para cima\n4- Andar para baixo\n')
+            }
             if(m==1){
                 try {
                     pos= await api.findIdPosicao(posicao.posicao_x +1 , posicao.posicao_y, posicao.nome) 
-                    console.log(pos.id)
                     await api.updatePosicao(pos.id, player.id)
                 } catch (error) {
                     console.log(error)
@@ -137,7 +142,6 @@ let username =''
             if(m==3){
                 try {
                     pos= await api.findIdPosicao(posicao.posicao_x , posicao.posicao_y+1, posicao.nome) 
-                    console.log(pos.id)
                     await api.updatePosicao(pos.id, player.id)
                 } catch (error) {
                     console.log(error)
@@ -155,10 +159,18 @@ let username =''
                 }
                 
             }
+            if(m==5){
+                try {
+                    pos= await api.findIdPosicao(0 , 0, regiao) 
+                    await api.updatePosicao(pos.id, player.id)
+                } catch (error) {
+                    console.log(error)
+                }
+                
+            }
         }
     }
 }
-
 main()
 
 
