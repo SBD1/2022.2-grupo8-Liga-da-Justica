43c3fff14afccc6ea55c53405e34939abb3931d1
MtsSrs <matheus.1503@hotmail.com>
2023-01-16 20:54:58 -0300

    Consultar nível do personagem

--- scripts/dql.sql
+++ scripts/dql.sql
@@ -2,3 +2,7 @@
 select tb_personagem.nome, tb_faccao.nome
 	from tb_personagem join tb_faccao
 		on tb_personagem.id_faccao = tb_faccao.id
+
+-- Ver nível do personagem
+select tb_personagem.nome, tb_personagem.nivel
+    from tb_personagem 
