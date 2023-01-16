d2518d03b15b376eec4e62d049d910e3697a4896
MtsSrs <matheus.1503@hotmail.com>
2023-01-16 20:57:41 -0300

    Consultar classe do personagem

--- scripts/dql.sql
+++ scripts/dql.sql
@@ -6,3 +6,8 @@ select tb_personagem.nome, tb_faccao.nome
 -- Ver nível do personagem
 select tb_personagem.nome, tb_personagem.nivel
     from tb_personagem 
+
+-- Ver classe do personagem
+select tb_personagem.nome, tb_classe.nome
+    from tb_personagem join tb_classe
+        on tb_personagem.id_classe = tb_classe.id
