20374d073218481524cd9806b242bbfdd3365da9
MtsSrs <matheus.1503@hotmail.com>
2023-01-16 21:04:30 -0300

    Consultar localização do personagem

--- scripts/dql.sql
+++ scripts/dql.sql
@@ -1,13 +1,18 @@
 -- Ver Facção do personagem
 select tb_personagem.nome, tb_faccao.nome
-	from tb_personagem join tb_faccao
+	from tb_personagem inner join tb_faccao
 		on tb_personagem.id_faccao = tb_faccao.id
-
+		
 -- Ver nível do personagem
 select tb_personagem.nome, tb_personagem.nivel
     from tb_personagem 
 
 -- Ver classe do personagem
 select tb_personagem.nome, tb_classe.nome
-    from tb_personagem join tb_classe
+    from tb_personagem inner join tb_classe
         on tb_personagem.id_classe = tb_classe.id
+      
+-- Ver localização do personagem
+select tb_personagem.nome, tb_regiao.nome, tb_regiao.posicao_x, tb_regiao.posicao_y 
+	from tb_personagem inner join tb_regiao
+		on tb_personagem.id_local_atual = tb_regiao.id 
