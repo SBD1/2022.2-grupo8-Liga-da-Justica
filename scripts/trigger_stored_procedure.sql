d13f273891ee2492c38fff00ff78e6eb157851b9
CorreiaJV <correia.jv16@gmail.com>
2023-01-28 17:08:26 -0300

    Trigger que cria tb_poder ao criar personagem

---
+++ scripts/trigger_stored_procedure.sql
@@ -0,0 +1,39 @@
+-- Função para criar personagens com poucos parâmetros 
+
+CREATE OR REPLACE FUNCTION criarPersonagem(nome varchar(30), sexo int, idClasse int, idFaccao int, idRaca int ) RETURNS VOID AS
+$$
+DECLARE
+idMentor int;
+begin
+	select tb_npc_mentor.id into idMentor from tb_npc_mentor where id_classe = idClasse and id_faccao = idFaccao;
+
+    insert into tb_personagem(experiencia, nivel, nome, QTD_PontosDeVida, MAX_PontoDeVida, sexo, QTD_PontosDeEstamina, MAX_PontosDeEstamina, QTD_Honra, QTD_Defesa, QTD_Ataque, id_poder, id_Local_Atual, id_ajudante, id_mentor, id_classe, id_faccao, id_raca) 
+   values (0, 1, nome, 100, 100, sexo , 100, 100, 50, 0, 0, 1 , 1, null, idMentor, idClasse , idFaccao , idRaca);
+
+END
+$$
+  LANGUAGE 'plpgsql';
+ 
+
+-- Função para criar a tabela de poder ao criar um personagem 
+
+CREATE OR REPLACE FUNCTION criarPoderPersonagem() RETURNS TRIGGER  AS
+$$
+DECLARE
+idPoder int;
+begin
+	
+	insert into tb_poderes(Velocidade, Furtividade, Magia, Forca, Carisma, Percepcao) values ('4', '4', '4', '4', '4', '4') RETURNING id into idPoder;
+	new.id_poder := idPoder;
+	
+return new;
+
+END
+$$
+  LANGUAGE 'plpgsql';
+
+--Trigger para inserção de personagem 
+
+CREATE TRIGGER  "tg_criaTbPoder " before INSERT 
+ON tb_personagem FOR EACH ROW 
+    EXECUTE function  criarPoderPersonagem();
